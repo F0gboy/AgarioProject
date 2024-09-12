cb170f89292eee1cc4dcd2806689783fbd64c436
Jasper Fog Pedersen <Jasp3r@live.dk>
2024-09-12 11:51:46 +0200

    simple chat sat op

--- AgarioProject/Program.cs
+++ AgarioProject/Program.cs
@@ -1,5 +1,3 @@
-using System.Net.Sockets;
-
 namespace AgarioProject
 {
 
@@ -9,36 +7,8 @@ namespace AgarioProject
         [STAThread]
         static void Main()
         {
-            TcpClient client = new TcpClient();
-            client.Connect("localhost", 12000);
-            Console.WriteLine("Connected to server...");
-            Thread receiveThread = new Thread(() => ReceiveMessages(client));
-            receiveThread.Start();
-
-           
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-
-            StreamWriter writer = new StreamWriter(client.GetStream());
-            while (true)
-            {
-                string message = Console.ReadLine();
-                writer.WriteLine(message);
-                writer.Flush();
-            }
-
-            void ReceiveMessages(TcpClient client)
-            {
-                StreamReader reader = new StreamReader(client.GetStream());
-                while (client.Connected)
-                {
-                    string message = reader.ReadLine();
-                    if (message != null)
-                    {
-                        Console.WriteLine(message);
-                    }
-                }
-            }
         }
     }
 }
---
+++ TempClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Sockets;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AgarioProject
+{
+
+    internal static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("localhost", 12000);
+            Console.WriteLine("Connected to server...");
+            Thread receiveThread = new Thread(() => ReceiveMessages(client));
+            receiveThread.Start();
+
+            StreamWriter writer = new StreamWriter(client.GetStream());
+            while (true)
+            {
+                string message = Console.ReadLine();
+                writer.WriteLine(message);
+                writer.Flush();
+            }
+
+            void ReceiveMessages(TcpClient client)
+            {
+                StreamReader reader = new StreamReader(client.GetStream());
+                while (client.Connected)
+                {
+                    string message = reader.ReadLine();
+                    if (message != null)
+                    {
+                        Console.WriteLine(message);
+                    }
+                }
+            }
+        }
+    }
+}
